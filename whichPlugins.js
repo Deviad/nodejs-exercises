c3a973b118705dd765b4d9a7d6b9d9c35e5cde78
Davide Pugliese <Deviad@users.noreply.github.com>
2018-02-24 14:24:02 +0200

    Create whichPlugins.js

---
+++ whichPlugins.js
@@ -0,0 +1,37 @@
+// This code is able to scan a JSON in order to find the keys on the left side and say if there are some matching a given list.
+
+let [executable, absPath, target, ...message] = process.argv;
+const fs = require('fs');
+const readStream = fs.createReadStream(target, {encoding:  "utf8"});
+
+
+const whichPlugins = (pluginList) => (function(){
+  let arr = [];
+  let activePlugins = [];
+
+  return new Promise((resolve, reject) => {
+    readStream
+    .on('readable', function () {
+      let chunk;
+      while (null !== (chunk = readStream.read())) {
+        const wordBuffer = chunk.split(/\s+/);
+        arr = wordBuffer
+        .filter(word=> word.match(/"[a-zA-z]+[a-zA-z0-9-_+=]*":/g))
+        .map(word=>word.split(':').join('').split('"').join(''));
+      }
+    });
+    readStream
+    .on('end', function () {
+      if(arr.length > 0 ) {
+        arr.map(el => { 
+          if (pluginList.indexOf(el) > -1) {
+            activePlugins.push(el);
+          } 
+        });
+      }
+      resolve({activePlugins});
+    });
+  });
+})(pluginList);
+
+whichPlugins(['pippo', 'pluto']).then(console.log);
